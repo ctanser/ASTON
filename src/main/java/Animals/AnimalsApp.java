c062d8d6dfc8c6711debfb89246885422141f8bb
ctanser <ctanser@yandex.ru>
2024-05-25 16:23:33 +0300

    Lesson 4 исправления

--- src/main/java/Animals/AnimalsApp.java
+++ src/main/java/Animals/AnimalsApp.java
@@ -30,7 +30,6 @@ public class AnimalsApp {
             cat.eat(cat, bowl, 22);
             System.out.println(cat.name + " сытость: " + cat.satiety);
         }
-
         //добавляем еды
         bowl.addEat(35);
         cats[2].eat(cats[2], bowl, 20);
