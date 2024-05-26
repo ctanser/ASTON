f201e25cff202902a7078c90af026ad3c21ed9b4
ctanser <ctanser@yandex.ru>
2024-05-27 01:05:22 +0300

    Lesson 4 Alfa

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String[][] array = {{"1", "2", "3", "4"}, {"5", "6", "7", "8"}, {"9", "10", "11", "12"}, {"13", "14", "15", "в"}};
+        String[][] array = {{"1", "2", "3", "4"}, {"5", "6", "7", "8"}, {"9", "10", "11", "12"}, {"13", "14", "15", "9"}};
 
         try {
             int sum = sumArray(array);
@@ -14,11 +14,11 @@ public class Main {
         int sum = 0;
 
         if (arr.length != 4) {
-            throw new MyArraySizeException("Неверный размер массива");
+            throw new MyArraySizeException("Строк в массиве должно быть 4");
         }
         for (int i = 0; i < arr.length; i++) {
             if (arr[i].length != 4) {
-                throw new MyArraySizeException("Неверный размер массива");
+                throw new MyArraySizeException("Столбцов в массиве должно быть 4");
             }
         }
 
@@ -27,7 +27,7 @@ public class Main {
                 try {
                     sum += Integer.parseInt(arr[i][j]);
                 } catch (NumberFormatException e) {
-                    throw new MyArrayDataException("Неверные данные в ячейке [" + i + "][" + j + "]");
+                    throw new MyArrayDataException("В ячейке [" + i + "][" + j + "] не целое число");
                 }
             }
         }
